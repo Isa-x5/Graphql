f09062304e3a0dcd72b5cecdddf6206c9f0c2c94
Isa-x5 <isa.habib1993@gmail.com>
2025-03-03 02:38:20 +0300

    new3

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 // next.config.js
 module.exports = {
   reactStrictMode: true,
+  basePath: '/Graphql',  // This will ensure your app is served from the /Graphql/ path
+  assetPrefix: '/Graphql/',  // This is used for assets to be loaded from the correct path
 };
