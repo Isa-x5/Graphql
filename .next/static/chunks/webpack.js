c23694a4a2b8b88c10d3206f27d66b90ec67d212
Isa-x5 <isa.habib1993@gmail.com>
2025-03-06 18:53:00 +0300

    updated css to be more responsive

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -160,7 +160,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("f385702a41bc80e7")
+/******/ 		__webpack_require__.h = () => ("45a25fee65a2ada6")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
